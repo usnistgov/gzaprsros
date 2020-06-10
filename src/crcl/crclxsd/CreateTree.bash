8944fe18d1332095018bcf25858a103ba4f91693
John Michaloski <john.michaloski@nist.gov>
2020-06-10 16:18:21 -0400

    added crcl documentation

---
+++ src/crcl/crclxsd/CreateTree.bash
@@ -0,0 +1,8 @@
+xsdcxx  cxx-tree --generate-polymorphic --generate-from-base-ctor --generate-serialization --polymorphic-type CRCLCommand --polymorphic-type-all    CRCLCommandInstance.xsd
+xsdcxx cxx-tree --generate-polymorphic --generate-from-base-ctor --generate-serialization --polymorphic-type CRCLCommand --polymorphic-type-all CRCLCommands.xsd  
+xsdcxx cxx-tree --generate-polymorphic --generate-from-base-ctor --generate-serialization --polymorphic-type-all CRCLProgramInstance.xsd  
+xsdcxx cxx-tree --generate-polymorphic --generate-from-base-ctor --generate-serialization --polymorphic-type-all CRCLStatus.xsd  
+xsdcxx cxx-tree --generate-polymorphic --generate-from-base-ctor --generate-serialization --polymorphic-type-all  DataPrimitives.xsd 
+
+
+
